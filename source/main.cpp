7167911552024c5dbad97ca0f8281adddca53100
John Parkhurst <jparkhurst120@gmail.com>
2022-01-03 00:05:58 -0500

    Setup regex for file ending

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
         exit(59);
     #endif
     //Check that argv[1] contains text file
-    if (regex_search (argv[1], std::regex("inputs") )){
+    if (regex_search (argv[1], std::regex(".txt$") )){
         std::cout << argv[1]<< " MATCHED THE REGEX\n";
     }else{
         std::cout << argv[1] << " NO MATCH FOUND FOR REGEX\n";
