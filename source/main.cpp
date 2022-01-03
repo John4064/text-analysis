87eeefbb01d8efbbfcfb201431af39884960feca
John Parkhurst <jparkhurst120@gmail.com>
2022-01-02 22:42:57 -0500

    Was using match not search byaccident

--- source/main.cpp
+++ source/main.cpp
@@ -11,8 +11,7 @@ int main(int argc, char *argv[]) {
         std::cout << "This Version is multithreaded \n";
     #endif
     //Check that argv[1] contains text file
-    std::string s = argv[1];
-    if (regex_match (s, std::regex("(../inputs/)(.*)") )){
+    if (regex_search (argv[1], std::regex("inputs") )){
         std::cout << argv[1]<< " MATCHED THE REGEX\n";
     }else{
         std::cout << argv[1] << " NO MATCH FOUND FOR REGEX\n";
