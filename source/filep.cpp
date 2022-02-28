a702ddb6301579d275e2a8e8f92b5ff7b87f2a65
cboyce376 <cboyce376@gmail.com>
2022-02-28 12:18:28 +1030

    Regex impletmented #18

--- source/filep.cpp
+++ source/filep.cpp
@@ -12,22 +12,23 @@ std::string stringConc(const char* filename){
     //open file
     return fileString;
 }
-std::string removeChar(std::string str, char bad){
-    str.erase(remove(str.begin(),str.end(),bad),str.end());
-    return str;
-}
+
+bool invalidChar (char c) 
+{  
+	//Checks if characters are A-Z, a-z, 0-9, "-", " ' " or space
+	//The characters above are the allowed characters
+
+	
+	
+	std::string inChar{c};
+	std::string  pattern = "[- 'a-zA-Z0-9]";
+	return !(std::regex_match(inChar, std::regex(pattern)));
+} 
+
 //Test
 std::string cleanup(std::string inWord) {
-    inWord = removeChar(inWord, '.');
-    inWord = removeChar(inWord, ',');
-    inWord = removeChar(inWord, '"');
-    inWord = removeChar(inWord, '?');
-    inWord = removeChar(inWord, ';');
-    inWord = removeChar(inWord, '!');
-    inWord = removeChar(inWord, '(');
-    inWord = removeChar(inWord, ')');
-    inWord = removeChar(inWord, ':');
-    //May remove numbers not sure.
+	//Remove all characters according to the invalidChar function above
+	inWord.erase(remove_if(inWord.begin(),inWord.end(),invalidChar),inWord.end());
 
     //Convert to all lowercase: .02 to .03 with 25 values printed runtime ideal
     transform (inWord.begin(), inWord.end(), inWord.begin(), ::tolower);
@@ -38,6 +39,7 @@ std::vector<std::string> processFil(const char* argv){
 
     //Open the File specified
     std::string filName = stringConc(argv);
+	std::cout << filName << std::endl;
     //Read the File
     std::ifstream fil(filName);
     std::string buffer;
