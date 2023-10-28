f4c5811ce4fd7a22487842e37e76472ceb170206
John Parkhurst <jparkhurst120@gmail.com>
2023-10-28 01:28:02 -0400

    Optimizing basic time stats
    Improved logging to differenciate between process time   & file input time

--- source/main.cpp
+++ source/main.cpp
@@ -8,7 +8,7 @@
 #include "computation.h"
 
 
-#define numThread 4
+#define numThread 1
 
 
 int main(int argc, char *argv[]) {
@@ -28,16 +28,18 @@ int main(int argc, char *argv[]) {
         exit(59);
     #endif
     std::cout << "Processing File As we Speak!\n";
-    //The beginning Step you may say! It opens and reads the book
+    auto start_read = std::chrono::high_resolution_clock::now();
+    //The beginning Step you may say! It opens and reads the book & takes the longest
     std::vector<std::string> bookVec =processFil(argv[1]);
-    //Here we gather the Statistics
-    auto start = std::chrono::high_resolution_clock::now();
-
+    auto stop_read = std::chrono::high_resolution_clock::now();
+    auto duration_read = std::chrono::duration_cast<std::chrono::microseconds>(stop_read - start_read);
+    //Here we gather the Statistics//In the report we output  Thread # and time efficiency
+    std::cout << "The file reading time for "<<numThread<<" threads was "<< duration_read.count()*.000001 << " Seconds"<<std::endl;
+    auto start_process = std::chrono::high_resolution_clock::now();
     processText(bookVec,numThread);
+    auto stop_process = std::chrono::high_resolution_clock::now();
+    auto duration_process = std::chrono::duration_cast<std::chrono::microseconds>(stop_process - start_process);
 
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    //In the report we output  Thread # and time efficiency
-    std::cout << "The Execution Time for "<<numThread<<" threads was "<< duration.count()*.000001 << " Seconds"<<std::endl;
+    std::cout << "The data processing time for "<<numThread<<" threads was "<< duration_process.count()*.000001 << " Seconds"<<std::endl;
     return 0;
 }
